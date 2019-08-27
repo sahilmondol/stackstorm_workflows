ae7347891942321fd4d41d39d92190293bdde59f
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-27 12:18:32 +0530

    miscelleneous changes

--- actions/greatest_number.py
+++ actions/greatest_number.py
@@ -1,6 +1,6 @@
 from st2common.runners.base_action import Action
 
-class greatest_number(Action):
+class smallest_number(Action):
 	def run(self, input1, input2):
 		print(input1+' is greater than '+input2)
 		print(message)
